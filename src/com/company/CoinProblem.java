a340c43684d8456fe3c6fd065eee3295b9015efd
Manuel Avila Rodriguez <mastercraft9000@gmail.com>
2021-05-12 21:52:06 +0000

    Created CoinProblem class and Knapsack class

---
+++ src/com/company/CoinProblem.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * An algorithm used to recursively construct a set of objects from the smallest
+ * possible constituent parts. Given a set of k integers (a_1, a_2, ..., a_k)
+ * with a_1<a_2<...<a_k, a greedy algorithm can be used to find a vector of
+ * coefficients (c_1, c_2, ..., c_k) such that sum_(i = 1)^k c_i a_i = c·a = n,
+ * where c·a is the dot product, for some given integer n.
+ */
+public class CoinProblem {
+
+}
