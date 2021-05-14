80cd82cb420a838342aa5d27091acc09f8506d92
Henry McElwayne <mastercraft9000@gmail.com>
2021-05-13 20:39:52 -0500

    Update Figure.java and Point.java
    Updated Figure and Point classes

--- src/com/company/Figure.java
+++ src/com/company/Figure.java
@@ -1,5 +1,25 @@
 package com.company;
 
 public class Figure {
-    
+    public Point[] points;
+
+    public Figure(Point[] points) {
+        this.points = points;
+    }
+
+    public void draw() {
+        //TODO: draw code
+    }
+
+    public void translate() {
+        //TODO: translate code
+    }
+
+    public void scalate() {
+        //TODO: scale code
+    }
+
+    public void rotate() {
+        //TODO: rotate code
+    }
 }
